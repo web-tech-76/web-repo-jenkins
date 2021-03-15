7b7edfaf4c1adb5f30fc2b5c0f1a950b93644c51
web_tech_76 <webtec76@gmail.com>
2021-03-15 15:01:11 +0530

    initial commit

---
+++ jenkins-app/src/test/java/jenkins/app/JenkinsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package jenkins.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
